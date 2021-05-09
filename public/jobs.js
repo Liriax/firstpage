c9e11547731fc8011d3d196b655f9df50b0f4ef4
Liria <liriaxiaoge@gmail.com>
2021-05-09 16:06:11 +0200

    update

--- public/jobs.js
+++
@@ -1,9 +0,0 @@
-
-const major_input = document.getElementById('major');
-const button = document.getElementById('submit');
-
-// $("submit").click(function(){
-//     $.get("/",function(data){
-//         alert("data"+data);
-//     });
-// });
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var session = require('express-session');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var http = require('http');
+const rp = require('request-promise');
+const otcsv = require('objects-to-csv');
+const cheerio = require('cheerio');
 
 var path = require('path');
 ///////////////////
 
-var axios = require('axios');
-const cheerio = require('cheerio');
 const csv = require('csv-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 // var connection = mysql.createConnection({
 // 	host     : 'localhost',
@@ -95,11 +95,13 @@ app.get('/home', function (request, response) {
 
 app.post('/opt', function (request, response) {
 	var major = request.body.major;
-	scrapeAndFilter (major);
-	sleep(1000);
+	var pages = request.body.pages;
+
+	var jobs=[];
 	var found_jobs = [];
-	const jobs=[];
-	fs.createReadStream('details.csv')
+	get_all_pages(pages).then(() => {
+		console.log('SUCCESSFULLY COMPLETED THE STELLENWERK SCRAPING SAMPLE');
+		fs.createReadStream('stellenwerk.csv')
 			.pipe(csv())
 			.on('data', (row) => {
 				jobs.push(row);
@@ -108,89 +110,102 @@ app.post('/opt', function (request, response) {
 
 				jobs.forEach(element => {
 					var requirements = element['Anforderungsprofil'];
-					if (requirements != undefined && requirements.includes(major)) {
+					if (requirements != undefined && requirements.toLowerCase().includes(major.toLowerCase())) {
 						found_jobs.push({
 							link: element['link'],
 							title: element['title']
 						});
 					}
 				});
-				console.log(found_jobs);
-				response.send(found_jobs);
-				return;
+				// console.log(found_jobs);
+				// response.send(found_jobs);
+
+				var fileName = __dirname + '/public' + '/result.html';
+				var stream = fs.createWriteStream(fileName);
+
+				stream.once('open', function(fd) {
+				var html = buildHtml();
+
+				stream.end(html);
+				});
+				
+				function buildHtml() {
+					var header = '<link rel="stylesheet" href="result.css">';
+					var body = '<table>';
+				  
+					for (var i = 0; i < found_jobs.length; i++) {
+						var tr = "<tr>";
+						
+						/* Must not forget the $ sign */
+						tr += "<td>" + i + "</td>" + "<td><a href=" + found_jobs[i]['link'].toString() + ">"+found_jobs[i]['title'].toString()+"</a>" + "</td></tr>";
+	
+						/* We add the table row to the table body */
+						body += tr;
+					}
+					body += '</table>'
+				  
+					return '<!DOCTYPE html>'
+						 + '<html><head>' + header + '</head><body>' + body + '</body></html>';
+				  }
+
+
+				response.redirect('/result.html');
+				response.end();
 			});
+	});
+
+	
 });
 
 app.listen(process.env.PORT || 5000);
 
-const csvWriter2=createCsvWriter({
-	path: 'details.csv',
-	header:[
-		{id: 'type', title: 'type'},
-		{id: 'title',title: 'title'},
-		{id: 'link',title: 'link'},
-		{id: 'Anforderungsprofil', title: 'Anforderungsprofil'},
-		{id:'Beschreibung',title:'Beschreibung'},
-		{id: 'frist', title: 'frist'}
-	]
-});
 
-function scrapeAndFilter (major){
-    for (page = 0; page<=3; page++ ){
-        const url = 'https://www.stellenwerk-darmstadt.de/jobboerse?keywords=&offer_type=All&job_category=All&employment_type=All&page='+page;
-        (function (url){
-            // console.log(page);
-            return axios.get(url)
-            .then(response=>{
-                let getData = (html) => {
-                    link=[];
-                    const $ = cheerio.load(html);
-                    $('.SearchListItem-titleWrapper > a').each((i,elem)=>{
-                        link.push($(elem).attr('href'));
-                    });
-                    return link;
-                }
-                return getData(response.data);
-                // csvWriter.writeRecords(link).catch(()=>console.log('error writing csv'));
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        })(url).then(data => {
-            data.forEach(element =>{
-                    const url = 'https://www.stellenwerk-darmstadt.de'+element;
-                    axios.get(url)
-                    .then(response=>{
-                        data=[];
-    
-                        let getData = html => {
-                            const $ = cheerio.load(html);
-                            $('.TwoColumn').each((i, elem)=>{
-                                data.push({
-                                    type: $(elem).find('.JobHead-additionalInfoItem:nth-child(3)').text(),
-                                    title: $(elem).find('.JobHead-title').text(),
-                                    link: url,
-                                    Anforderungsprofil: $(elem).find('.Title:contains("Anforderungsprofil") ~ div > ul').text(),
-                                    Beschreibung: $(elem).find('.Title:contains("Beschreibung") ~ div').text(),
-                                    frist: $(elem).find('.JobDescriptionList-term:contains("Bewerbungsfristsende") ~ dd').text()
-                                })
-                            });
-          
-                            // console.log(data);
-                        };
-                        getData(response.data);
-                        csvWriter2.writeRecords(data).catch(()=>console.log('error writing csv'));
-    
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            });
+
+const baseURL = 'https://www.stellenwerk-darmstadt.de';
+const searchURL = '/jobboerse?keywords=&offer_type=All&job_category=All&employment_type=All&page=';
+
+const getJobs = async (page) => {
+    const html = await rp(baseURL + searchURL+page);
+    const $ = cheerio.load(html);
+    const businessMap = $('.SearchListItem-titleWrapper > a').map(async (i, e) => {
+      const link = baseURL + $(e).attr('href');
+      const innerHtml = await rp(link);
+      const $_inner = cheerio.load(innerHtml);                         
+      const Anforderungsprofil = $_inner('.Title:contains("Anforderungsprofil") ~ div > ul').text();
+      const type = $_inner('.JobHead-additionalInfoItem:nth-child(3)').text();
+      const title = $_inner('.JobHead-title').text();
+      const Beschreibung = $_inner('.Title:contains("Beschreibung") ~ div').text();
+      const frist = $_inner('.JobDescriptionList-term:contains("Bewerbungsfristsende") ~ dd').text();
+
+      return {
+        type,
+        title,
+        link,
+        Anforderungsprofil,
+        Beschreibung,
+        frist
+      }
+    }).get();
+    return Promise.all(businessMap);
+};
+
+const get_all_pages = async (page_num) => {
+    var all_results=[];
+    for (var n=0; n<=page_num; n++){
+        await getJobs(n)
+        .then(result => {
+            all_results = all_results.concat(result);
+            //console.log(all_results.length);
         });
     }
+    
+    const transformed = new otcsv(all_results);
+    return transformed.toDisk('./stellenwerk.csv');
+};
 
-}
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+
+
+
+
+
